0ae6f9cf7c7d8d08b75eb67320b5d21a8ac73eba
Pedro Andrade <pedro.andrade@inpe.br>
2016-01-29 09:29:10 -0200

    Initial commit.

---
+++ description.lua
@@ -0,0 +1,9 @@
+version = "0.1"
+license = "LGPL-3"
+date = "29 January 2016"
+package = "game"
+title = "Games"
+authors = "Pedro R. Andrade"
+contact = "pedro.andrade@inpe.br"
+content = [[This package contains models that use concepts from Game Theory.
+]]
